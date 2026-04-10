981fa9d8ea545601917df1b0997ac412c4c51011
wishMeLuck9 <149300259+wishMeLuck9@users.noreply.github.com>
2026-04-10 13:05:52 +0100

    pref

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+
+    void Start()
+    {
+        Instantiate(enemyPrefab, new Vector3(0, 0, 6), enemyPrefab.transform.rotation);
+    }
+}
