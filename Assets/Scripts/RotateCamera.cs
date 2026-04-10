4e6f883d7dc9d703b7b642975cbd739467470199
wishMeLuck9 <149300259+wishMeLuck9@users.noreply.github.com>
2026-04-10 12:29:55 +0100

    Update RotateCamera.cs

--- Assets/Scripts/RotateCamera.cs
+++ Assets/Scripts/RotateCamera.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
 
 public class RotateCamera : MonoBehaviour
+
 {
+    public float rotationSpeed = 20f; // Speed of rotation
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal"); // Get horizontal input (A/D or Left/Right arrow keys)
+        // Rotate the camera around the Y-axis
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
     }
 }
